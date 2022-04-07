df21a37406de8fe85479c03fe28f9d229c70f2ac
Vasco Santos <santos.vasco10@gmail.com>
2022-04-07 12:06:22 +0200

    feat: website release please (#12)

--- packages/website/pages/index.js
+++ packages/website/pages/index.js
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
@@ -16,7 +15,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">nftstorage.link!</a>
+          Welcome to <a href="https://nextjs.org">nftstorage.link</a>!
         </h1>
 
         <p className={styles.description}>
