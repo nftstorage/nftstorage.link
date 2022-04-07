32f32e2ecc5b45aaddc3d93b56131bbe4ced8558
Vasco Santos <santos.vasco10@gmail.com>
2022-04-07 11:42:01 +0200

    fix: website cloudflare pages build

--- packages/website/next.config.js
+++ packages/website/next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'out',
+  images: {
+    loader: 'custom',
+  },
 }
 
 module.exports = nextConfig
