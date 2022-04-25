a4cf7c723ff756d74a10715fa3b73f2ab6004575
Alan Shaw <alan.shaw@protocol.ai>
2022-04-25 12:56:07 +0100

    fix: make work and add tests

--- packages/client/src/lib.js
+++ packages/client/src/lib.js
@@ -10,7 +10,7 @@ export class StatusChecker {
   /**
    * @param {Object} [config]
    * @param {string} [config.endpoint] Gateway status API URL.
-   * @param {string} [config.maxAge] Time in ms after which a status value is stale.
+   * @param {number} [config.maxAge] Time in ms after which a status value is stale.
    * @param {globalThis.fetch} [config.fetch] Custom fetch API implementation.
    */
   constructor(config) {
@@ -47,22 +47,41 @@ export class StatusChecker {
 const defaultStatusChecker = new StatusChecker()
 
 /**
- * Get a gateway URL, given a CID, CID+path or a gateway URL. If the status of
- * the nftstorage.link gateway is known to be good (according to the status
- * checker) then return a URL that uses nftstorage.link, otherwise return an URL
- * that uses dweb.link (or the optional passed fallback gateway URL).
+ * Get a gateway URL, given a CID, CID+path, IPFS path or an IPFS gateway URL.
+ * If the status of the nftstorage.link gateway is known to be good (according
+ * to the status checker) then return a URL that uses nftstorage.link, otherwise
+ * return an URL that uses dweb.link (or the optional passed fallback gateway
+ * URL).
  *
  * @param {string|URL} cidPath
  * @param {Object} [options]
  * @param {StatusChecker} [options.statusChecker]
- * @param {string} [options.fallbackGatewayUrl]
+ * @param {string} [options.fallbackGatewayURL]
  */
 export async function getGatewayURL(cidPath, options) {
+  /* c8 ignore next 2 */
   options = options || {}
   const statusChecker = options.statusChecker || defaultStatusChecker
-  const fallbackGatewayUrl = options.fallbackGatewayUrl || FALLBACK_GATEWAY_URL
+  const fallbackGatewayURL = options.fallbackGatewayURL || FALLBACK_GATEWAY_URL
   const isOk = await statusChecker.ok()
-  const { pathname, search, hash } = new URL(cidPath, GATEWAY_URL)
-  const base = isOk ? GATEWAY_URL : fallbackGatewayUrl
+  let { protocol, hostname, pathname, search, hash } = new URL(
+    cidPath,
+    GATEWAY_URL
+  )
+  if (protocol === 'ipfs:' || protocol === 'ipns:') {
+    // differences in parsing behaviour between Node.js and Chrome URL:
+    // in one impl, hostname is 'bafyrei...', pathname is '/path' and in the
+    // other, hostname is '', pathname is '//bafyrei.../path'.
+    /* c8 ignore next 1 */
+    pathname = hostname ? `/${hostname}${pathname}` : pathname.slice(1)
+    pathname = `/${protocol.slice(0, -1)}${pathname}`
+  } else if (!pathname.startsWith('/ipfs') && !pathname.startsWith('/ipns')) {
+    const hostParts = hostname.split('.') // subdomain gateway URL?
+    pathname =
+      hostParts[1] === 'ipfs' || hostParts[1] === 'ipns'
+        ? `/${hostParts[1]}/${hostParts[0]}${pathname === '/' ? '' : pathname}`
+        : `/ipfs${pathname}`
+  }
+  const base = isOk ? GATEWAY_URL : fallbackGatewayURL
   return new URL(`${pathname}${search}${hash}`, base).toString()
 }
