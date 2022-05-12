b10a13d3c1d577cec75728a2da5bf5ea1defec5a
Vasco Santos <santos.vasco10@gmail.com>
2022-05-12 11:50:56 +0200

    fix: prometheus header rename

--- packages/edge-gateway/src/metrics.js
+++ packages/edge-gateway/src/metrics.js
@@ -233,8 +233,8 @@ export async function metricsGet(request, env, ctx) {
       (gw) =>
         `nftgateway_winner_requests_total{gateway="${gw}",env="${env.ENV}"} ${metricsCollected.ipfsGateways[gw].totalWinnerRequests}`
     ),
-    `# HELP nftgateway_requests_per_time_total total of successful requests per response time bucket`,
-    `# TYPE nftgateway_requests_per_time_total histogram`,
+    `# HELP nftgateway_successful_requests_per_time_total total of successful requests per response time bucket`,
+    `# TYPE nftgateway_successful_requests_per_time_total histogram`,
     ...responseTimeHistogram.map((t) => {
       return env.ipfsGateways
         .map(
